631b17164cba00a0d6e466fd77088d127198f83f
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-21 15:13:40 +0100

    get likes and find stored channel video

--- config_example.php
+++ config_example.php
@@ -1,15 +1,5 @@
 <?php
 
 return [
-    'api' => [
-        'client_id' => '',
-        'client_secret' => '',
-        'refresh_token' => ''
-    ],
-    'db' => [
-        'host' => '',
-        'database' => 'youtube_channel_infos',
-        'username' => '',
-        'password' => ''
-    ]
+    'apiToken' => ''
 ];
--- src/App.php
+++ src/App.php
@@ -2,10 +2,74 @@
 
 namespace App;
 
+use DomDocument;
+
 class App
 {
     public function run(): void
     {
-        echo 'test';
+        $config = require
+            __DIR__
+            . DIRECTORY_SEPARATOR
+            . '..'
+            . DIRECTORY_SEPARATOR
+            . 'config.php'
+        ;
+        $token = $config['apiToken'];
+        
+        $likeCurl = curl_init('https://old.miniggiodev.fr/api/getVideoableLikes.php');
+
+        curl_setopt_array($likeCurl, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => ['Content-Type: application/json' , 'Authorization: Bearer ' . $token]
+        ]);
+
+        $likeCurlResponse = curl_exec($likeCurl);
+
+        if ($likeCurlResponse === false) {
+            return;
+        }
+
+        $likes = json_decode($likeCurlResponse, true);
+
+        if ($likes === null) {
+            return;
+        }
+
+        $channelVideos = [];
+
+        $channelStorageUrl = 'https://storage.miniggiodev.fr/youtube-likes-recap/channel/';
+
+        foreach ($likes as &$like) {
+            $channelId = $like['channel_id'];
+
+            if (! in_array($channelId, array_keys($channelVideos))) {
+                $channelVideoCurl = curl_init($channelStorageUrl . $channelId . '/');
+                curl_setopt($channelVideoCurl, CURLOPT_RETURNTRANSFER, true);
+                $channelVideoCurlResponse = curl_exec($channelVideoCurl);
+                $httpCode = curl_getinfo($channelVideoCurl)['http_code'];
+
+                if ($httpCode === 200) {
+                    $dom = new DomDocument();
+                    $dom->loadHTML($channelVideoCurlResponse);
+                    $links = $dom->getElementsByTagName('a');
+
+                    $videos = [];
+                    foreach ($links as $link) {
+                        $content = $link->textContent;
+                        if (str_contains($content, '.webm')) {
+                            $videos[] = substr($content, 0, -5);
+                        }
+                    }
+
+                    $channelVideos[$channelId] = $channelStorageUrl . $channelId . '/' . $videos[array_rand($videos)];
+                } else {
+                    $channelVideos[$channelId] = 'test';
+                    // TODO make placeholder video
+                }
+            }
+
+            $like['channel_video'] = $channelVideos[$channelId];
+        }
     }
 }
