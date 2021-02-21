c087cbd9246714e74a66a29ea9d8d4ef7b0e8d1c
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-21 23:47:22 +0100

    move code to like populator

--- src/App.php
+++ src/App.php
@@ -2,10 +2,9 @@
 
 namespace App;
 
-use DomDocument;
-
 class App
 {
+
     public function run(): void
     {
         $config = require
@@ -36,83 +35,7 @@ class App
             return;
         }
 
-        $supportedLangs = ['fr', 'en'];
-
-        $speechToTextAPI = 'https://gtts-api.miniggiodev.fr';
-        $supportedLangsCurl = curl_init($speechToTextAPI);
-        curl_setopt($supportedLangsCurl, CURLOPT_RETURNTRANSFER, true);
-        $supportedLangsCurlResponse = curl_exec($supportedLangsCurl);
-
-        if (! empty($supportedLangsCurlResponse)) {
-            $supportedLangsCurlJsonResponse = json_decode($supportedLangsCurlResponse, true);
-
-            if (! empty($supportedLangsCurlJsonResponse) && ! empty($supportedLangsCurlJsonResponse['langs'])) {
-                $supportedLangs = $supportedLangsCurlJsonResponse['langs'];
-            }
-        }
-
-        $channelVideos = [];
-        $channelAudios = [];
-
-        $channelStorageUrl = 'https://storage.miniggiodev.fr/youtube-likes-recap/channel/';
-
-        foreach ($likes as &$like) {
-            $channelId = $like['channel_id'];
-
-            $channelVideo = null;
-
-            if (! in_array($channelId, array_keys($channelVideos))) {
-                $channelVideoCurl = curl_init($channelStorageUrl . $channelId . '/');
-                curl_setopt($channelVideoCurl, CURLOPT_RETURNTRANSFER, true);
-                $channelVideoCurlResponse = curl_exec($channelVideoCurl);
-                $httpCode = curl_getinfo($channelVideoCurl)['http_code'];
-
-                if ($httpCode === 200) {
-                    $dom = new DomDocument();
-                    $dom->loadHTML($channelVideoCurlResponse);
-                    $links = $dom->getElementsByTagName('a');
-
-                    $videos = [];
-                    foreach ($links as $link) {
-                        $content = $link->textContent;
-                        $videoExt = '.webm';
-                        if (str_contains($content, $videoExt)) {
-                            $videos[] = substr($content, 0, - strlen($videoExt));
-                        }
-                    }
-
-                    $channelVideo = $channelStorageUrl . $channelId . '/' . $videos[array_rand($videos)];
-                    $channelVideos[$channelId] = $channelVideo;
-                }
-            } else {
-                $channelVideo = $channelVideos[$channelId];
-            }
-
-            $like['channel_video'] = $channelVideo;
-
-            if (! in_array($channelId, array_keys($channelAudios))) {
-                $lang = 'en';
-
-                if (! empty($like['channel_country'])) {
-                    $langCurl = curl_init('https://country-to-lang-api.miniggiodev.fr/' . $like['channel_country']);
-                    curl_setopt($langCurl, CURLOPT_RETURNTRANSFER, true);
-                    $curlResponse = curl_exec($langCurl);
-
-                    if (! empty($curlResponse)) {
-                        $jsonResponse = json_decode($curlResponse, true);
-                        if (! empty($jsonResponse) && $jsonResponse['lang']) {
-                            $fetchedLang = $jsonResponse['lang'];
-
-                            if (in_array($fetchedLang, $supportedLangs)) {
-                                $lang = $fetchedLang;
-                            }
-                        }
-                    }
-                }
-                $channelAudios[$channelId] = $speechToTextAPI . '/' . urlencode($like['channel_name']) . '?lang=' . $lang;
-            }
-
-            $like['channel_audio'] = $channelAudios[$channelId];
-        }
+        $likePopulator = new LikePopulator();
+        $likePopulator->populate($likes);
     }
 }
