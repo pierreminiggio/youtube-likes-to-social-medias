822031f026eb40ab588c744f513028b8e9dd45be
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-04-23 03:49:02 +0200

    done ???

--- src/App.php
+++ src/App.php
@@ -99,20 +99,20 @@ class App
 
         $videoFile = $videoFolder . DIRECTORY_SEPARATOR . 'out.mp4';
 
-        // if (file_exists($videoFile)) {
-        //     echo PHP_EOL . PHP_EOL . 'Removing old video file ...';
-        //     unlink($videoFile);
-        //     echo ' Removed !';
-        // }
+        if (file_exists($videoFile)) {
+            echo PHP_EOL . PHP_EOL . 'Removing old video file ...';
+            unlink($videoFile);
+            echo ' Removed !';
+        }
 
         echo PHP_EOL . PHP_EOL . 'Rendering video ...';
-        // $renderLog = shell_exec('npm --prefix ' . escapeshellarg($videoFolder) . ' run build');
+        $renderLog = shell_exec('npm --prefix ' . escapeshellarg($videoFolder) . ' run build');
 
-        // if (! str_contains($renderLog, 'Your video is ready')) {
-        //     echo ' Error while rendering !';
+        if (! str_contains($renderLog, 'Your video is ready')) {
+            echo ' Error while rendering !';
 
-        //     return;
-        // }
+            return;
+        }
 
         echo ' Rendered !';
 
@@ -159,7 +159,7 @@ class App
         echo PHP_EOL . PHP_EOL . 'Uploading to Youtube ...';
 
         $videoPoster = (new VideoPosterFactory())->make(new Logger());
-        $videoPoster->post(
+        $youtubeVideoId = $videoPoster->post(
             $config['heropostLogin'],
             $config['heropostPassword'],
             $config['channelId'],
@@ -183,32 +183,93 @@ class App
 
         echo ' Uploaded !';
 
+        echo PHP_EOL . PHP_EOL . 'Tweeting ...';
+        
+        $tweetStart = 'J\'ai 👍 les videos de ';
+        $tweetEnd = ' :' . PHP_EOL . 'https://youtu.be/' . $youtubeVideoId;
+
+        $twitterHandles = [];
+        $tweet = '';
+        foreach ($likes as &$like) {
+            $twitterCurl = curl_init('https://twitter-handle-finder-api.miniggiodev.fr/' . $like['channel_id']);
+            curl_setopt($twitterCurl, CURLOPT_RETURNTRANSFER, true);
+            $twitterCurlResponse = curl_exec($twitterCurl);
+            curl_close($twitterCurl);
+
+            if (empty($twitterCurlResponse)) {
+                continue;
+            }
+
+            $jsonTwitterCurlResponse = json_decode($twitterCurlResponse, true);
+
+            if ($jsonTwitterCurlResponse === null || empty($jsonTwitterCurlResponse['twitter_handle'])) {
+                continue;
+            }
+
+            $twitterHandle = $jsonTwitterCurlResponse['twitter_handle'];
+
+            if (in_array($twitterHandle, $twitterHandles)) {
+                continue;
+            }
+
+            $twitterHandles[] = $twitterHandle;
+
+            $maybeNextTweet =
+                $tweetStart
+                . implode(
+                    ' ',
+                    array_map(fn (string $handle): string => '@' . $handle, $twitterHandles)
+                )
+                . $tweetEnd;
+
+            if (strlen($maybeNextTweet) >= 280) {
+                break;
+            }
+
+            $tweet = $maybeNextTweet;
+        }
+
+        if (! $twitterHandles) {
+            $tweet = $tweetStart . 'plusieurs Youtubeurs' . $tweetEnd;
+        }
+
+        $postTweetCurl = curl_init('https://old.miniggiodev.fr/test/twitter/api/index.php');
+        $PostTweetCurlOptions = $curlOptions;
+        $PostTweetCurlOptions[CURLOPT_POST] = 1;
+        $PostTweetCurlOptions[CURLOPT_POSTFIELDS] = $tweet;
+        curl_setopt_array($postTweetCurl, $PostTweetCurlOptions);
+        curl_exec($postTweetCurl);
+        curl_close($postTweetCurl);
+
+        echo ' Tweeted !';
+
         echo PHP_EOL . PHP_EOL . 'Mark as videoed ...';
 
-        // $setAsVideoedCurl = curl_init($apiBaseUrl . '/setAsVideoed.php');
+        $setAsVideoedCurl = curl_init($apiBaseUrl . '/setAsVideoed.php');
 
-        // $curlOptions[CURLOPT_POST] = 1;
-        // $curlOptions[CURLOPT_POSTFIELDS] = json_encode([
-        //     'ids' => array_map(fn (array $like): int => (int) $like['id'], $likes)
-        // ]);
+        $videoableCurlOptions = $curlOptions;
+        $videoableCurlOptions[CURLOPT_POST] = 1;
+        $videoableCurlOptions[CURLOPT_POSTFIELDS] = json_encode([
+            'ids' => array_map(fn (array $like): int => (int) $like['id'], $likes)
+        ]);
 
-        // curl_setopt_array($setAsVideoedCurl, $curlOptions);
+        curl_setopt_array($setAsVideoedCurl, $videoableCurlOptions);
 
-        // $setAsVideoedCurlResponse = curl_exec($setAsVideoedCurl);
+        $setAsVideoedCurlResponse = curl_exec($setAsVideoedCurl);
 
-        // if ($setAsVideoedCurlResponse === false) {
-        //     echo 'Set as videoable failed : Erreur curl';
+        if ($setAsVideoedCurlResponse === false) {
+            echo 'Set as videoable failed : Erreur curl';
 
-        //     return;
-        // }
+            return;
+        }
 
-        // $httpCode = curl_getinfo($setAsVideoedCurl)['http_code'];
+        $httpCode = curl_getinfo($setAsVideoedCurl)['http_code'];
 
-        // if ($httpCode !== 204) {
-        //     echo 'Set as videoable failed : not a 204';
+        if ($httpCode !== 204) {
+            echo 'Set as videoable failed : not a 204';
 
-        //     return;
-        // }
+            return;
+        }
 
         echo ' Marked !';
     }
